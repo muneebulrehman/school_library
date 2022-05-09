1553cdf2e56d87d1d8918a63562185f05e9cd99a
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-09 17:31:27 +0530

    Fixed linter errors

--- teacher.rb
+++ teacher.rb
@@ -3,10 +3,9 @@ class Teacher < Person
   attr_accessor :specialization
 
   def initialize(specialization, name, age, parental_permission)
-    super(name, age, parental_permission )
+    super(name, age, parental_permission)
     @specialization = specialization
   end
-  
   def can_use_services
     true
   end
