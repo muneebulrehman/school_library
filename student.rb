d278213c20267ed218f3c909139b28d099c6da90
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-09 17:15:32 +0530

    Added teacher.rb file with teacher class

--- student.rb
+++ student.rb
@@ -4,8 +4,8 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(classroom, name, age, can_use_services)
-    @classroom = classroom
     super(name, age, can_use_services)
+    @classroom = classroom
   end
 
   def play_hooky
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,16 @@
 require relative './person.rb'
 
 class Teacher < Person
-  
 
+  attr_accessor :specialization
+
+  def initialize(specialization, name, age, parental_permission)
+    super( name, age, parental_permission )
+    @specialization = specialization
+  end
+  
+  def can_use_services
+    true
+  end
 
 end
