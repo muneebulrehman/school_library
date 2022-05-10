d5a9f9517b30816e2f33da744b7138fb61e7eda2
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-10 18:07:05 +0530

    Added baseDecorator class

--- nameable.rb
+++ nameable.rb
@@ -3,7 +3,17 @@ class Nameable
   def correct_name
     raise NotImplementedError
   end
-
-  
-
 end
+
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
